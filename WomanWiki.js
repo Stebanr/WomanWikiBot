6ca62c1150bc4daf80772b0f174b8ba03eac2bad
Stebanr <38438641+Stebanr@users.noreply.github.com>
2018-04-16 13:26:47 -0500

    Create WomanWiki.js

---
+++ WomanWiki.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+   console.log("Estoy listo!");
+});
+var prefix = config.prefix;
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix)) return;
+  if (message.author.bot) return;
+  
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix +"embed")) {
+    const embed = new Discord.RichEmbed()
+    .setDescription("Airi es una ninja femenina que puede desgarrar fácilmente al equipo enemigo con una movilidad excepcional.");
+
+    message.channel.send({embed});
+  }
+});
+client.login(process.env.BOT_TOKEN);
